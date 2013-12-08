4b246d82122ddcb64e82758a596be4eff81cd66b
Gurudath Bn <gurudath.1989@gmail.com>
2013-12-08 15:03:44 +0530

    status

--- app/controllers/sitemaps_controller.rb
+++ app/controllers/sitemaps_controller.rb
@@ -1,14 +1,14 @@
 class SitemapsController < ApplicationController
   caches_page :index
   def index
-    @posts = Article.all[0..10]
+    @posts = Article.all
     respond_to do |format|
       format.xml
     end
   end
 
   def article_sitemap
-    @posts = Article.all[0..10]
+    @posts = Article.all
     respond_to do |format|
       format.xml
     end
